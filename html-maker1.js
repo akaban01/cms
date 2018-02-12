4ed1c6fb5566dd3b26158669ce61cf409bb28b16
Faizan Rauf <faizan.rauf@deluxe.com>
2018-02-12 15:54:38 -0600

    more stuff

---
+++ html-maker1.js
@@ -0,0 +1,32 @@
+var page_array = [["[jumbotron]","[jumbotron]"],["#Welcome to the site","![Welcome banner](http://daxushequ.com/data/out/24/img60426245.jpg)"],["this site is all driven by a spreadsheet",""],["",""],["[card]","[card]"],["card heading","![Welcome banner](http://daxushequ.com/data/out/24/img60426245.jpg)"],["some text",""]];
+
+var currentComponentRow = 0;
+var totalComponentRows = 0;
+
+var row = 0;
+var col = 0;
+var result = "";
+
+function printHTML(){
+	setTotalNumberOfComponentRows();
+	for(var i = 0; i < totalComponentRows; i++){
+		//start container, row and col
+		//start component, row and col
+		//print everything in current col
+		//if in current component row next col is empty -- awesome step
+			//close col and start next one -- then goto awesome step
+		//else
+			//close col, row, component
+		//close col, row, container
+		
+		//--------------
+	}
+}
+
+function setNextComponent(){
+	
+}
+
+function setTotalNumberOfComponentRows(){
+	
+}
