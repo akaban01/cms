1801947ff171f8cd3d3b12ef29cdec6c3c6c5aae
Faizan Rauf <faizan.rauf@deluxe.com>
2018-02-13 08:56:12 -0600

    getting there step by step

--- html-maker1.js
+++ html-maker1.js
@@ -1,28 +1,49 @@
 var page_array = [["[jumbotron]","[jumbotron]"],["#Welcome to the site","![Welcome banner](http://daxushequ.com/data/out/24/img60426245.jpg)"],["this site is all driven by a spreadsheet",""],["",""],["[card]","[card]"],["card heading","![Welcome banner](http://daxushequ.com/data/out/24/img60426245.jpg)"],["some text",""]];
 
 var currentComponentRow = 0;
-var totalComponentRows = 0;
+var componentRows = [];
 
 var row = 0;
-var col = 0;
+var col = -1;
+
 var result = "";
 
 function printHTML(){
 	setTotalNumberOfComponentRows();
-	for(var i = 0; i < totalComponentRows; i++){
+	for(var i = 0; i < componentRows.length; i++){
 		//start container, row and col
-		//start component, row and col
+		//start component, row 
+		//start col -- awesome step
 		//print everything in current col
-		//if in current component row next col is empty -- awesome step
+		//close col
+		//if in current component row next col is empty 
 			//close col and start next one -- then goto awesome step
 		//else
-			//close col, row, component
+			//close col, row, component -- then goto awesome step
 		//close col, row, container
 		
 		//--------------
+		result+=section_start+row_start+col_start;
+		result+=component_start+row_start;
+		for(var j = -1; j < componentRows[i].length; j++){
+			if(isNextColEmpty(componentRows[i],j)){
+				printColContent();
+			}
+			else{
+				result+=row_end;
+				result+=component_end;
+				result+=component_start+row_start;
+				printColContent();
+				//now go to if here
+			}
+		}
 	}
 }
-
+function printColContent(){
+	result+=col_start;
+	result+=getComponentsHTML();
+	result+=col_end;
+}
 function setNextComponent(){
 	
 }
